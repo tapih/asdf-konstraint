7a1ffe882628f0a4a9361a80dd0ea714311e7dca
Hiroshi Muraoka <h.muraoka714@gmail.com>
2024-06-23 10:59:46 +0900

    clear TODO
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for konstraint.
 GH_REPO="https://github.com/plexsystems/konstraint"
 TOOL_NAME="konstraint"
 TOOL_TEST="konstraint --help"
@@ -31,19 +30,19 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if konstraint has other means of determining installable versions.
 	list_github_tags
 }
 
 download_release() {
 	local version filename url
+	platform="$(get_platform)"
+	arch="$(get_arch)"
+	ext="$(get_ext)"
+
 	version="$1"
-	filename="$2"
+	filename="$2${ext}"
 
-	# TODO: Adapt the release URL convention for konstraint
-	url="$GH_REPO/archive/v${version}.tar.gz"
-
+	url="$GH_REPO/releases/download/v${version}/konstraint-${platform}-${arch}${ext}"
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
@@ -61,7 +60,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert konstraint executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
@@ -71,4 +69,42 @@ install_version() {
 		rm -rf "$install_path"
 		fail "An error occurred while installing $TOOL_NAME $version."
 	)
+}
+
+get_platform() {
+	platform="$(uname | tr '[:upper:]' '[:lower:]')"
+
+	echo -n "$platform"
+}
+
+get_arch() {
+	arch="$(uname -m | tr '[:upper:]' '[:lower:]')"
+
+	case "$arch" in
+	x86_64)
+		arch="amd64"
+		;;
+	arm64)
+		arch='arm64'
+		;;
+	aarch64)
+		arch='arm64'
+		;;
+	*)
+		echo "Unsupported arch: $arch" >&2
+		exit 1
+		;;
+	esac
+
+	echo -n "$arch"
+}
+
+get_ext() {
+	platform="$(uname | tr '[:upper:]' '[:lower:]')"
+
+	if [ "$platform" == "windows" ]; then
+		echo -n ".exe"
+	else
+		echo -n ""
+	fi
 }
